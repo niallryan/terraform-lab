ac324a794b350386b755358fa151b2c81685b474
Rondineli <raraujo04@yahoo.com.br>
2017-04-08 21:11:41 +0100

    removed unsed vars

--- variables.tf
+++ variables.tf
@@ -8,36 +8,6 @@ variable "access_key" {
 variable "secret_key" {
   default=""
 }
-
-variable "ip_range" {
-  default = "192.168.0.0/16"
-}
-variable "availability_zones" {
-  default = "us-west-2a,us-west-2b,us-west-2c"
-}
-variable "key_name" {
-  default = "pop"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "asg_min" {
-  default = "2"
-}
-variable "asg_max" {
-  default = "5"
-}
-variable "asg_desired" {
-  default = "1"
-}
-
-variable "amis" {
-  default = {
-    us-east-1 = "ami-01146c31"
-    us-west-2 = "ami-01146c31"
-  }
-}
-
 variable "vpc_cidr" {
   description = "My CiDR VPC"
   default = "10.0.0.0/16"
